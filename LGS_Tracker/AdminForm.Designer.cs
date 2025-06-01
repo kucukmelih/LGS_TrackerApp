da65168b075cfbc94097358282f04c11a79297dc
Melih Küçük <146946480+kucukmelih@users.noreply.github.com>
2025-06-01 14:52:32 +0300

    Add files via upload

---
+++ LGS_Tracker/AdminForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace LGS_Tracker
+{
+    partial class AdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnStudentList = new System.Windows.Forms.Button();
+            this.btnAddExam = new System.Windows.Forms.Button();
+            this.btnViewAllResults = new System.Windows.Forms.Button();
+            this.btnOCRPDF = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnReport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(342, 190);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(489, 51);
+            this.lblTitle.TabIndex = 8;
+            this.lblTitle.Text = "Admin Panel - Home Page";
+            // 
+            // btnStudentList
+            // 
+            this.btnStudentList.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnStudentList.Location = new System.Drawing.Point(408, 266);
+            this.btnStudentList.Name = "btnStudentList";
+            this.btnStudentList.Size = new System.Drawing.Size(352, 58);
+            this.btnStudentList.TabIndex = 9;
+            this.btnStudentList.Text = "Student List";
+            // 
+            // btnAddExam
+            // 
+            this.btnAddExam.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnAddExam.Location = new System.Drawing.Point(408, 346);
+            this.btnAddExam.Name = "btnAddExam";
+            this.btnAddExam.Size = new System.Drawing.Size(352, 58);
+            this.btnAddExam.TabIndex = 10;
+            this.btnAddExam.Text = "Add Exam to Student";
+            // 
+            // btnViewAllResults
+            // 
+            this.btnViewAllResults.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnViewAllResults.Location = new System.Drawing.Point(408, 426);
+            this.btnViewAllResults.Name = "btnViewAllResults";
+            this.btnViewAllResults.Size = new System.Drawing.Size(352, 58);
+            this.btnViewAllResults.TabIndex = 11;
+            this.btnViewAllResults.Text = "View All Exam Results";
+            // 
+            // btnOCRPDF
+            // 
+            this.btnOCRPDF.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnOCRPDF.Location = new System.Drawing.Point(408, 584);
+            this.btnOCRPDF.Name = "btnOCRPDF";
+            this.btnOCRPDF.Size = new System.Drawing.Size(352, 58);
+            this.btnOCRPDF.TabIndex = 14;
+            this.btnOCRPDF.Text = "Data Entry via PDF / OCR";
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnLogout.Location = new System.Drawing.Point(408, 658);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(352, 58);
+            this.btnLogout.TabIndex = 15;
+            this.btnLogout.Text = "Log Out";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.lblMessage.Location = new System.Drawing.Point(73, 62);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(119, 32);
+            this.lblMessage.TabIndex = 16;
+            this.lblMessage.Text = "Welcome";
+            // 
+            // btnReport
+            // 
+            this.btnReport.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnReport.Location = new System.Drawing.Point(408, 507);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(352, 58);
+            this.btnReport.TabIndex = 17;
+            this.btnReport.Text = "Download PDF";
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1176, 836);
+            this.Controls.Add(this.btnReport);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnStudentList);
+            this.Controls.Add(this.btnAddExam);
+            this.Controls.Add(this.btnViewAllResults);
+            this.Controls.Add(this.btnOCRPDF);
+            this.Controls.Add(this.btnLogout);
+            this.Name = "AdminForm";
+            this.Text = "AdminForm";
+            this.Load += new System.EventHandler(this.AdminForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnStudentList;
+        private System.Windows.Forms.Button btnAddExam;
+        private System.Windows.Forms.Button btnViewAllResults;
+        private System.Windows.Forms.Button btnOCRPDF;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnReport;
+    }
+}
---
+++ LGS_Tracker/AdminForm.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using LGS_Tracker;
+
+namespace LGS_Tracker
+{
+    public partial class AdminForm : Form
+    {
+        public AdminForm()
+        {
+            InitializeComponent();
+
+            // Event subscriptions
+            this.Load += AdminForm_Load;
+            btnAddExam.Click += btnAddExam_Click;
+            btnStudentList.Click += btnStudentList_Click;
+            btnViewAllResults.Click += btnViewAllResults_Click;
+            btnReport.Click += btnReport_Click;
+            btnOCRPDF.Click += btnOCRPDF_Click;
+            btnLogout.Click += btnLogout_Click;
+
+            // Apply hover effects to all buttons
+            ApplyHoverEffect(btnAddExam);
+            ApplyHoverEffect(btnStudentList);
+            ApplyHoverEffect(btnViewAllResults);
+            ApplyHoverEffect(btnReport);
+            ApplyHoverEffect(btnOCRPDF);
+            ApplyHoverEffect(btnLogout);
+        }
+
+        // When form loads, apply theme and display welcome message
+        private void AdminForm_Load(object sender, EventArgs e)
+        {
+            lblMessage.Text = "Welcome, Admin!";
+            ApplyTheme();
+        }
+
+        // Apply colors and styles to form controls
+        private void ApplyTheme()
+        {
+            this.BackColor = Color.FromArgb(240, 248, 255);
+
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is Label)
+                {
+                    ctrl.ForeColor = Color.Black;
+                }
+                else if (ctrl is TextBox || ctrl is ComboBox)
+                {
+                    ctrl.BackColor = Color.White;
+                    ctrl.ForeColor = Color.Black;
+                }
+                else if (ctrl is Button btn)
+                {
+                    // Special theme for logout button
+                    if (btn == btnLogout)
+                    {
+                        btn.BackColor = Color.FromArgb(255, 99, 99);
+                        btn.ForeColor = Color.White;
+                        btn.FlatStyle = FlatStyle.Flat;
+                        btn.FlatAppearance.BorderSize = 1;
+                        btn.FlatAppearance.BorderColor = Color.DarkRed;
+                    }
+                    else
+                    {
+                        btn.BackColor = Color.FromArgb(100, 149, 237); // Cornflower Blue
+                        btn.ForeColor = Color.Black;
+                        btn.FlatStyle = FlatStyle.Flat;
+                        btn.FlatAppearance.BorderSize = 1;
+                        btn.FlatAppearance.BorderColor = Color.FromArgb(200, 200, 200);
+                    }
+                }
+            }
+        }
+
+        // Add hover behavior for each button
+        private void ApplyHoverEffect(Button btn)
+        {
+            btn.MouseEnter += (s, e) =>
+            {
+                if (btn == btnLogout)
+                    btn.BackColor = Color.FromArgb(220, 20, 60); // Crimson
+                else
+                    btn.BackColor = Color.FromArgb(70, 130, 180); // Steel Blue
+            };
+
+            btn.MouseLeave += (s, e) =>
+            {
+                if (btn == btnLogout)
+                    btn.BackColor = Color.FromArgb(255, 99, 99);
+                else
+                    btn.BackColor = Color.FromArgb(100, 149, 237); // Cornflower Blue
+            };
+        }
+
+        // Remove any open child forms before opening a new one
+        private void ClearEmbeddedForms()
+        {
+            foreach (var ctrl in this.Controls.OfType<Form>().ToList())
+            {
+                ctrl.Close();
+                this.Controls.Remove(ctrl);
+            }
+        }
+
+        // Show a given form embedded within the main AdminForm panel
+        private void ShowEmbeddedForm(Form form)
+        {
+            ClearEmbeddedForms();
+
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.Dock = DockStyle.Fill;
+            this.Controls.Add(form);
+            form.BringToFront();
+            form.Show();
+        }
+
+        // Open AddExamForm
+        private void btnAddExam_Click(object sender, EventArgs e)
+        {
+            ShowEmbeddedForm(new AddExamForm());
+            lblMessage.Text = "Add Exam Page Loaded";
+        }
+
+        // Open StudentListForm
+        private void btnStudentList_Click(object sender, EventArgs e)
+        {
+            ShowEmbeddedForm(new StudentListForm());
+            lblMessage.Text = "Student List Page Loaded";
+        }
+
+        // Open AdminExamViewer form
+        private void btnViewAllResults_Click(object sender, EventArgs e)
+        {
+            ShowEmbeddedForm(new AdminExamViewer());
+            lblMessage.Text = "All Results Page Loaded";
+        }
+
+        // Open ReportViewer form
+        private void btnReport_Click(object sender, EventArgs e)
+        {
+            ShowEmbeddedForm(new ReportViewer());
+            lblMessage.Text = "Report Page Loaded";
+        }
+
+        // Open OCR/PDF input form
+        private void btnOCRPDF_Click(object sender, EventArgs e)
+        {
+            ShowEmbeddedForm(new OCRandPDFInputForm());
+            lblMessage.Text = "OCR/PDF Page Loaded";
+        }
+
+        // Logout and return to LoginForm
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+        }
+    }
+}
